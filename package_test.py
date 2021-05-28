10de1564be8147d8aeea314a4b434bfb49b089cf
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-05-27 20:50:34 -0400

    testing successful

--- package_test.py
+++ package_test.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import BIDS_converter.data2bids as d2b
-import preprocess as prep
+from BIDS_coding.BIDS_converter.data2bids import Data2Bids
 import os
 import shutil
 
 def test_1():
 
-    os.mkdir("BIDS_converter/testing/BIDS")
-    d2b.Data2Bids(input_dir='BIDS_converter/testing/D52', output_dir='BIDS_converter/testing/BIDS').run()
+    try:
+        os.mkdir("BIDS_converter/samples/BIDS")
+    except FileExistsError:
+        pass
+    Data2Bids(input_dir='BIDS_converter/samples/D52', output_dir='BIDS_converter/samples/BIDS').run()
 
     xlist =[]
 
-    for x in os.listdir('BIDS_converter/testing/BIDS/sub-D52'):
+    for x in os.listdir('BIDS_converter/samples/BIDS/sub-D0052'):
         #os.close(x)
         xlist.append(x)
 
-    while os.path.isdir("BIDS_converter/testing/BIDS"):
-        shutil.rmtree("BIDS_converter/testing/BIDS", ignore_errors=True)
+    while os.path.isdir("BIDS_converter/samples/BIDS"):
+        shutil.rmtree("BIDS_converter/samples/BIDS", ignore_errors=True)
 
     return xlist
 
 def test_2():
 
-    os.mkdir("BIDS_converter/testing/BIDS")
-    d2b.Data2Bids(input_dir='BIDS_converter/testing/D48', output_dir='BIDS_converter/testing/BIDS').run()
+    try:
+        os.mkdir("BIDS_converter/samples/BIDS")
+    except FileExistsError:
+        pass
+    Data2Bids(input_dir='BIDS_converter/samples/D48', output_dir='BIDS_converter/samples/BIDS').run()
 
     xlist =[]
 
-    for x in os.listdir('BIDS_converter/testing/BIDS/sub-D48'):
+    for x in os.listdir('BIDS_converter/samples/BIDS/sub-D0048'):
         #os.close(x)
         xlist.append(x)
 
-    while os.path.isdir("BIDS_converter/testing/BIDS"):
-        shutil.rmtree("BIDS_converter/testing/BIDS", ignore_errors=True)
+    while os.path.isdir("BIDS_converter/samples/BIDS"):
+        shutil.rmtree("BIDS_converter/samples/BIDS", ignore_errors=True)
 
     return xlist
