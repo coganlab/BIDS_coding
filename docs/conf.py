10aa9a9e88b25cb7824e9f2dd8a17e1bd2fe156e
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-02-05 17:27:11 -0500

    added configuration

---
+++ docs/conf.py
@@ -0,0 +1,82 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('..'))
+
+
+import time
+import warnings
+import gc
+
+# -- Project information -----------------------------------------------------
+
+project = 'BIDS_coding'
+copyright = '2024, Aaron Earle-Richardson'
+author = 'Aaron Earle-Richardson'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['myst_parser',
+              'sphinx.ext.duration',
+              'sphinx.ext.doctest',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.linkcode',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.mathjax']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['tests']
+
+
+# -- Options for HTML output -------------------------------------------------
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+modindex_common_prefix = ["ieeg"]
+
+pygments_style = "sphinx"
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": 4,
+    "logo_only": False,
+}
+html_logo = "./images/brain_logo_blue.png"
+html_favicon = "./images/favicon.ico"
+
+html_context = {
+    # Enable the "Edit in GitHub link within the header of each page.
+    "display_github": True,
+    # Set the following variables to generate the resulting github URL for each page.
+    # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/
+    # {{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
+    "github_user": "coganlab",
+    "github_repo": "BIDS_coding",
+    "github_version": "main/",
+    "conf_py_path": "docs/",
+}
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. Pipelines documentation master file, created by
+   sphinx-quickstart on Thu Feb 23 01:25:16 2023.
+   This file has been adapted to provide a more readable and helpful homepage.
+
+Welcome to the IEEG_Pipelines Documentation
+===========================================
+
+This is the main page of the IEEG_Pipelines documentation. Here you will find the table of contents and links to other important resources.
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+
+Indices and Tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+* `MATLAB Module Index <mat-modindex.html>`_
+
+Thank you for using IEEG_Pipelines!
