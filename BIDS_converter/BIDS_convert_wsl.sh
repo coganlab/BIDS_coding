092aabc44048dfbaff80b110ae2de9cc63b73f36
Jim Zhang <jzhang7345@gmail.com>
2024-05-14 16:14:21 -0400

    make box for mac on config.json

--- BIDS_converter/BIDS_convert_wsl.sh
+++ BIDS_converter/BIDS_convert_wsl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ORIG_DATA_DIR="$HOME/Library/CloudStorage/Box-Box/CoganLab"
+OUTPUT_DIR="$ORIG_DATA_DIR/BIDS-1.1_GlobalLocal"
 TASKS=("GlobalLocal")
 #(D3 D5 D6 D7 D8 D9 D12 D14 D15 D16 D17 D18 D20 D22 D23 D24 D26 D27 D28 D29 D30 D31 D32 D53
 # D57 D59 D60 D61 D65 D66 D69 D70 D71 D72 D73)
@@ -15,7 +16,7 @@ for TASK in "${TASKS[@]}"
  do
     mapfile -t SUB_IDS < <(find "$ORIG_DATA_DIR/D_Data/$TASK" -maxdepth 1 -type d -name "D*" -exec basename {} \;)
     SUB_IDS=(D57)
-    OUTPUT_DIR="$HOME/Workspace/$TASK"
+    # OUTPUT_DIR="$ORIG_DATA_DIR/$TASK"
     BIDS_DIR="$OUTPUT_DIR/BIDS"
     ZIP=false
 
