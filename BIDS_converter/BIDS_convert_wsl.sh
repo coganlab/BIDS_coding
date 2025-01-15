77b83eb2745171ac1105b5a049f5aee0bc76a9c6
Baishenliang <liangbs95@gmail.com>
2025-01-15 16:38:16 -0500

    updated parameters to fit the lexical no delay tasks

--- BIDS_converter/BIDS_convert_wsl.sh
+++ BIDS_converter/BIDS_convert_wsl.sh
@@ -3,9 +3,9 @@
 ORIG_DATA_DIR="$HOME/Box/CoganLab"
 # OUTPUT_DIR="$ORIG_DATA_DIR/BIDS-1.1_GlobalLocal"
 OUTPUT_DIR="$HOME/Workspace/Lexical"
-TASKS=("LexicalDecRepDelay")
+TASKS=("LexicalDecRepNoDelay")
 # SUB_IDS=(D42 D44 D47)
-SUB_IDS=(D42)
+SUB_IDS=(D57)
 
 #declare -l mylist[30]
 
@@ -31,7 +31,7 @@ for TASK in "${TASKS[@]}"
     # mkdir -p $BIDS_DIR
     mkdir -p "$OUTPUT_DIR/stimuli"
     # shellcheck disable=SC2038
-    find "$ORIG_DATA_DIR/task_stimuli" -iname "LexicalDecRepDelay" -type d -exec echo "{}/." \; | xargs -I{} cp -afv {} "$OUTPUT_DIR/stimuli/"
+    find "$ORIG_DATA_DIR/task_stimuli" -iname "Lexical_No_Delay" -type d -exec echo "{}/." \; | xargs -I{} cp -afv {} "$OUTPUT_DIR/stimuli/"
     TASKLOWER=$(echo $TASK | tr '[:upper:]' '[:lower:]')
     #echo "$ORIG_DATA_DIR/task_stimuli/$TASKLOWER/."
     #cp -av "$ORIG_DATA_DIR/task_stimuli/$TASKLOWER/." "$BIDS_DIR/stimuli/"
