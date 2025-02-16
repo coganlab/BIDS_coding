82c1f74fa94d52278c6f6e457ff5de001afe84fc
Baishenliang <liangbs95@gmail.com>
2025-02-16 16:27:29 -0500

    updated LexicalNodelay

--- BIDS_converter/BIDS_convert_wsl.sh
+++ BIDS_converter/BIDS_convert_wsl.sh
@@ -4,8 +4,8 @@ ORIG_DATA_DIR="$HOME/Box/CoganLab"
 # OUTPUT_DIR="$ORIG_DATA_DIR/BIDS-1.1_GlobalLocal"
 OUTPUT_DIR="$HOME/Workspace/Lexical"
 TASKS=("LexicalDecRepNoDelay")
-# SUB_IDS=(D42 D44 D47)
-SUB_IDS=(D69)
+SUB_IDS=(D90 D92 D94 D100)
+# SUB_IDS=(D54)
 
 #declare -l mylist[30]
 
